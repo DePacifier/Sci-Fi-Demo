e3db4c1455d7481c870ec6dafe8f23d57e905668
DePacifier <milkybekele@gmail.com>
2021-01-11 12:05:34 +0300

    Player
    A simple player created, camera reoriented to player and applied movement controls and mouse camera pan capability

---
+++ Assets/Game/Scripts/LookY.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookY : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _sensitivity = 1.5f;
+    [SerializeField]
+    private float _inverted = -1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _mouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.x += _mouseY * _sensitivity * _inverted;
+        transform.localEulerAngles = newRotation;
+    }
+}
